c8003de5f497cea58f5da85f24c4ba7f8b1fb1b7
thomasnilsson <s144470@student.dtu.dk>
2018-03-29 18:46:29 +0200

    Test

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-let body = d3.select("body")
-let content = body.select("#panelDiv")
+let content = d3.select("body").select("#buttons")
 
 let addWeekButton = (w) => content.append("a")
-	.attr("class", "btn btn-primary btn-block")
 	.text("Week " + w)
 	.attr("href", "week" + w)
+	.append("p")
 
 let addAssignmentButton = (a) => content.append("a")
-	.attr("class", "btn btn-success btn-block")
 	.text("Assignment " + a)
 	.attr("href", "A" + a)
+	.append("p")
 
 let assignments = [1,2,3]
 let weeks = [1,2,3,4,5,6,7,8,9,10]
--- server.py
+++
@@ -1,6 +0,0 @@
-# Host web server with port 8887
-python -m http.server 8887 &
-
-# Clear port again if it's being used (it shouldn't be)
-sudo lsof -i :8887
-sudo kill -9 <PID>
