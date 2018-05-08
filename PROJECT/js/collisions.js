a300d01fb5aa7569f858e3267ef5d5cc05971357
thomasnilsson <s144470@student.dtu.dk>
2018-05-08 21:44:48 +0200

    choropleth and histogram

--- PROJECT/js/collisions.js
+++ PROJECT/js/collisions.js
@@ -12,8 +12,105 @@
 	}
 	var innerPadding = 0.1
 
-	//Define quantize scale to sort data values into buckets of color
-    let color = ['#27586B', '#003549', '#05425A', '#52707C', '#888B8C']
+
+	var histogramData = []
+
+	for (var i = 1; i < 10; i++) {
+		var obj = {
+			"x": i,
+			"a": 6 * i,
+			"b": 3 * i,
+			"c": 2 * i
+		}
+		histogramData.push(obj)
+	}
+
+	console.log(histogramData)
+
+	let yMax = d3.max(histogramData, d => d.a)
+
+	console.log(yMax)
+
+	let colors = ["red", "orange", "steelblue"]
+
+	var svg = d3.select("body").select("#container2")
+		.append("svg")
+		.attr("width", w2)
+		.attr("height", h2)
+
+	// x Scale
+	x = d3.scaleBand()
+		.domain(histogramData.map(d => d.x))
+		.rangeRound([boundaries.left, boundaries.right])
+		.paddingInner(innerPadding)
+
+	xAxis = d3.axisBottom(x).ticks(7)
+
+	// y Scale
+	y = d3.scaleLinear()
+		.domain([0, yMax])
+		.range([boundaries.bottom, boundaries.top])
+
+	yAxis = d3.axisLeft(y).ticks(5)
+
+	// Bars
+
+	svg.selectAll(".pedestrian")
+		.data(histogramData)
+		.enter()
+		.append("rect").attr("class", "pedestrian")
+		.attr("x", d => x(d.x))
+		.attr("y", d => y(d.a))
+		.attr("width", x.bandwidth())
+		.attr("height", d => boundaries.bottom - y(d.a))
+		.attr("fill", "#555")
+
+	svg.selectAll(".bike")
+		.data(histogramData)
+		.enter()
+		.append("rect").attr("class", "bike")
+		.attr("x", d => x(d.x) + x.bandwidth() / 4)
+		.attr("y", d => y(d.b))
+		.attr("width", x.bandwidth() / 2)
+		.attr("height", d => boundaries.bottom - y(d.b))
+		.attr("fill", "#888")
+
+	svg.selectAll(".car")
+		.data(histogramData)
+		.enter()
+		.append("rect").attr("class", "car")
+		.attr("x", d => x(d.x) + (x.bandwidth() / 2.6))
+		.attr("y", d => y(d.c))
+		.attr("width", x.bandwidth() / 4)
+		.attr("height", d => boundaries.bottom - y(d.c))
+		.attr("fill", "#EEE")
+
+	// Make x axis with a g-element
+	svg.append("g")
+		.attr("transform", "translate(0, " + (boundaries.bottom) + ")")
+		.call(xAxis)
+
+	// Make y axis with another g-element
+	svg.append("g")
+		.attr("id", "yAxis")
+		.attr("transform", "translate(" + boundaries.left + ", 0)")
+		.call(yAxis)
+
+	// Text label for the y axis
+	svg.append("text")
+		.attr("transform", "rotate(-90)")
+		.style("text-anchor", "middle")
+		.attr("y", boundaries.left/2 - 10)
+		.attr("x", -h / 4)
+		.text("Injured or killed")
+
+	// Text label for the y axis
+	svg.append("text")
+		// .attr("transform", "rotate(-90)")
+		.style("text-anchor", "middle")
+		.attr("y", boundaries.bottom + 40)
+		.attr("x", w/3)
+		.text("Hour of the Day")
 
 	//For converting Dates to strings
 	var hourOfDay = time => {
@@ -32,7 +129,7 @@
 	var projection = d3.geoMercator()
 		.center([-73.94, 40.70])
 		.translate([w / 2, h / 2])
-		.scale(55 * 1000)
+		.scale(45 * 1000)
 
 	//Define path generator
 	var path = d3.geoPath()
@@ -44,9 +141,13 @@
 		.attr("width", w)
 		.attr("height", h)
 
+	let colorFunc = x => "rgb(" + x + "," + x + "," + x + ")"
+
 	//Load in GeoJSON data
-	d3.json("data/boroughs.geojson", (error, json) => {
+	d3.json("data/zipcodes.geojson", (error, json) => {
 		if (error) console.log("error fetching data")
+
+		console.log(json)
 
 		//Bind data and create one path per GeoJSON feature
 		svgGeo.selectAll("path")
@@ -54,43 +155,33 @@
 			.enter()
 			.append("path")
 			.attr("d", path)
-			.style("fill", (d, i) => color[i])
+			.style("fill", d => colorFunc(d.id))
 			.style("stroke", "black")
 
-		svgGeo.selectAll("text")
-			.data(json.features)
-			.enter()
-			.append("text")
-			.attr("class", "label")
-			.attr("x", d => path.centroid(d)[0] - 30)
-			.attr("y", d => path.centroid(d)[1])
-			.style("fill", "black")
-			.text(d => d.properties.BoroName)
+		// d3.csv("data/nyc_collisions.csv", parseRow, data => {
 
-		d3.csv("data/nyc_collisions.csv", parseRow, data => {
+		// 	let goodData = data.filter(d => d.date && d.lat && d.lon)
+		// 	let chunk = goodData.filter((d, i) => i < 10000)
 
+		// 	console.log(chunk)
 
-			let goodData = data.filter(d => d.date && d.lat && d.lon)
-			let chunk = goodData.filter((d, i) => i < 10000)
+		// 	// var data2018 = data.filter(d => new Date(d.date).getFullYear() == 2018)
 
-			console.log(chunk)
+		// 	// console.log(data2018)
 
-			// var data2018 = data.filter(d => new Date(d.date).getFullYear() == 2018)
-
-			// console.log(data2018)
-
-			var circles = svgGeo.append("g")
-				.selectAll("circle")
-				.data(chunk)
-				.enter()
-				.append("circle")
-				.attr("cx", d => projection([d.lon, d.lat])[0])
-				.attr("cy", d => projection([d.lon, d.lat])[1])
-				.attr("r", 1)
-				.style("stroke-width", 0.25)
-				.style("opacity", 0.75)
-				.style("fill", "red")
-		})
+		// 	var circles = svgGeo.append("g")
+		// 		.selectAll("circle")
+		// 		.data(chunk)
+		// 		.enter()
+		// 		.append("circle")
+		// 		.attr("cx", d => projection([d.lon, d.lat])[0])
+		// 		.attr("cy", d => projection([d.lon, d.lat])[1])
+		// 		.attr("r", 1)
+		// 		.style("stroke-width", 0.25)
+		// 		.style("opacity", 0.75)
+		// 		.style("fill", "red")
+		// })
 
 	})
+
 })()
